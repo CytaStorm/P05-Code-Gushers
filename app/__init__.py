cd5735e493a28195370f8434778ba7dde5dc2190
Jeff Chen <cytastorm@gmail.com>
2023-05-31 14:02:28 -0400

    added bg image

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)    #create Flask object
 
+@app.route('/')
+def index():
+    return render_template('candy.html')
+
 if __name__ == "__main__": #false if this file imported as module
     app.debug = True 
     app.run(host='0.0.0.0')
