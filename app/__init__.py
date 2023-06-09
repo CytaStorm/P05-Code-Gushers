1af5e8113c5dc5d7bcfb82c1876a03de7ec2f940
Jeff <cytastorm@gmail.com>
2023-06-09 16:47:08 -0400

    work session

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)    #create Flask object
 def index():
     return render_template('candy.html')
 
+@app.route('/candy2')
+def endless():
+    return render_template('candy2.html')
+
 if __name__ == "__main__": #false if this file imported as module
     app.debug = True 
     app.run(host='0.0.0.0')
