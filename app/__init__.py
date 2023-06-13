fd3c8222ae56cd9a4b3255cf57ec91f3cee04cf0
zeeyingg <zjian30@stuy.edu>
2023-06-12 23:02:43 -0400

    CSS/HTML Changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)    #create Flask object
 
-@app.route('/')
+@app.route('/candy')
 def index():
     return render_template('candy.html')
 
@@ -10,9 +10,9 @@ def index():
 def endless():
     return render_template('candy2.html')
 
-#@app.route('/')
-#def home():
-#    return render_template('home.html')
+@app.route('/')
+def home():
+   return render_template('home.html')
 
 if __name__ == "__main__": #false if this file imported as module
     app.debug = True 
